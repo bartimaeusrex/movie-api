e323ee18907e29ffdd4e58b670ccd3cce671e466
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-21 13:35:23 -0400

    Satisfies conditions in the README, albeit unsophisticatedly

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,3 @@
-//
-// PULLING TEMPLATE FROM NFL API! CHANGE IT TO REFLECT MOVIE DATA!
-//
 
 const movies = require('../movies')
 
@@ -8,40 +5,68 @@ const getAllMovies = (request, response) => {
   return response.send(movies)
 }
 
+// const isPartialMatch = (request, response, next) => {
+//   if (!request.params) return response.sendStatus(400)
+
+//   request.params = request.params.includes()
+//   next()
+// }
+
 const getMovieByTitle = (request, response) => {
   const { title } = request.params
 
-  const matchingMovie = movies.find((movie) => movie.title === title)
+  const matchingMovie = movies.filter(movie => {
+    const isPartialMatch = (movie.directors.concat(movie.title)).join().toLowerCase()
 
-  return matchingMovie
-    ? response.send(matchingMovie)
-    : response.sendStatus(404)
+      return isPartialMatch.includes(title.toLowerCase())
+    })
+  return matchingMovie.length
+    ? response.send(matchingMovie) // || response.send(isPartialMatch)
+    : response.status(404).send('Can\'t find that movie.')
 }
 
-// Below is silly, because there are no numbers in this set of data
-// (particularly lastTitle + 1)
-// what do I need to do instead, if the title is missing?
-// Probably just error out: "A title is required."
-// So! 
-const getNewTitle = () => {
-  const lastTitle = movies.reduce((acc, movie) => {
-    return movie.title > acc ? movie.title : acc
-  }, 0)
+// ////
 
-  return lastTitle + 1
-}
+// const getMovieByDirector = (request, response) => {
+//   const { directors } = request.params
+
+// // have to search the ARRAY, not just the string!!
+// // also: should return ALL of that directors films, ideally.
+// // Returning them ALL would take a filter()...
+//   const matchingDirector = movies.find((movie) =>
+//     movie.directors.toLowerCase() === directors.toLowerCase())
+
+//   return matchingDirector
+//     ? response.send(matchingDirector)
+//     : response.status(404).send('Can\'t find that movie.')
+// }
+
+//
+// to account for partials, .includes() seems the likeliest tactic.
+// 
+// const matchingMovie = movies.find((movie) =>
+//   movie.title.toLowerCase() === title.toLowerCase() )
+// sentence.includes(word.toLowerCase())
+// 
+//   const partialMatch = movies.find((movie) => {
+//   movie.title.includes(title)
+// return matchingMovie
+// }
+
+
+// /////////////////////////////////////////////////////////////////////////////
 
 const saveNewMovie = (request, response) => {
   const {
-    title , directors, releaseDate, rating, runTime, genres
+    title, directors, releaseDate, rating, runTime, genres
   } = request.body
 
-  if (!title || !directors || !releaseDate || !rating || !runTime || !genres ) {
+  if ( !title || !directors || !releaseDate || !rating || !runTime || !genres ) {
     return response.status(400).send('All fields are required.')
   }
 
   const newMovie = {
-    title, directors, releaseDate, rating, runTime, genres, title: getNewTitle()
+    title, directors, releaseDate, rating, runTime, genres
   }
 // functionally:
 // movies = movies.concat([newMovie])
