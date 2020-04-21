84f76c7e80b64b31f17c6f25324318123a5c6277
bartimaeusrex <mrpatrickreinhart@gmail.com>
2020-04-21 13:43:53 -0400

    Removed comments

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,14 +5,7 @@ const getAllMovies = (request, response) => {
   return response.send(movies)
 }
 
-// const isPartialMatch = (request, response, next) => {
-//   if (!request.params) return response.sendStatus(400)
-
-//   request.params = request.params.includes()
-//   next()
-// }
-
-const getMovieByTitle = (request, response) => {
+const getMovieByKeyword = (request, response) => {
   const { title } = request.params
 
   const matchingMovie = movies.filter(movie => {
@@ -21,40 +14,9 @@ const getMovieByTitle = (request, response) => {
       return isPartialMatch.includes(title.toLowerCase())
     })
   return matchingMovie.length
-    ? response.send(matchingMovie) // || response.send(isPartialMatch)
+    ? response.send(matchingMovie)
     : response.status(404).send('Can\'t find that movie.')
 }
-
-// ////
-
-// const getMovieByDirector = (request, response) => {
-//   const { directors } = request.params
-
-// // have to search the ARRAY, not just the string!!
-// // also: should return ALL of that directors films, ideally.
-// // Returning them ALL would take a filter()...
-//   const matchingDirector = movies.find((movie) =>
-//     movie.directors.toLowerCase() === directors.toLowerCase())
-
-//   return matchingDirector
-//     ? response.send(matchingDirector)
-//     : response.status(404).send('Can\'t find that movie.')
-// }
-
-//
-// to account for partials, .includes() seems the likeliest tactic.
-// 
-// const matchingMovie = movies.find((movie) =>
-//   movie.title.toLowerCase() === title.toLowerCase() )
-// sentence.includes(word.toLowerCase())
-// 
-//   const partialMatch = movies.find((movie) => {
-//   movie.title.includes(title)
-// return matchingMovie
-// }
-
-
-// /////////////////////////////////////////////////////////////////////////////
 
 const saveNewMovie = (request, response) => {
   const {
@@ -68,12 +30,10 @@ const saveNewMovie = (request, response) => {
   const newMovie = {
     title, directors, releaseDate, rating, runTime, genres
   }
-// functionally:
-// movies = movies.concat([newMovie])
-// ...but that would mess with the movies variable at the very top.
+
   movies.push(newMovie)
 
   return response.status(201).send(newMovie)
 }
 
-module.exports = { getAllMovies, getMovieByTitle, saveNewMovie }
+module.exports = { getAllMovies, getMovieByKeyword, saveNewMovie }
--- index.js
+++ index.js
@@ -1,29 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { getAllMovies, getMovieByTitle, saveNewMovie }
+const { getAllMovies, getMovieByKeyword, saveNewMovie }
   = require('./controllers/movies')
 const app = express()
 
-// 1) A request for all data: DONE.
 app.get('/movies', getAllMovies)
 
-// -------> Good place for an: app.use(isPartialMatch())
-
-// 3) A request for a movie by title
-// ...but need to figure out how to get at words in the string
-// which is done by .split(), I believe...
-// eg /lord gets you The Lord of the Rings
-app.get('/movies/:title', getMovieByTitle)
-
-// and then same goes for ...
-// 2) A REQUEST BY ALL MOVIES BY A DIRECTOR
-
-// app.get('/movies/:directors', getMovieByDirector)
-
-// eg /darabont gets you all Frank Darabont's movies
-// eg all the objects that contain "darabont" in their directors
-// will be returned. ...'Course, Darabont only directed one. Better use Spielberg.
-// of course, all of this will probably be in the controllers file! :)
+app.get('/movies/:title', getMovieByKeyword)
 
 app.post('/movies', bodyParser.json(), saveNewMovie)
 
